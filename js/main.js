a322a6066c045dab828821b400f18b23b971cb46
RafaelaMichelin <Rafaelamichelin@gmail.com>
2025-05-06 10:05:48 -0300

    Implementando efeito Fade-in e adicionando footer

--- js/main.js
+++ js/main.js
@@ -54,3 +54,36 @@ console.log(count);
 
 
 
+//Efeito fade- in ao rolar a página
+document.addEventListener('DOMContentLoaded', () => {
+    const faders = document.querySelectorAll('.fade-in');
+  
+    const appearOptions = {
+      threshold: 0.1,
+      rootMargin: "0px 0px -50px 0px"
+    };
+  
+    const appearOnScroll = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (!entry.isIntersecting) return;
+  
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target); // remove observação depois de animar
+      });
+    }, appearOptions);
+  
+    faders.forEach(fader => {
+      appearOnScroll.observe(fader);
+    });
+  });
+
+
+  //efeito fade-in ao carregar a pagina no banner
+  window.addEventListener('load', () => {
+    const banner = document.querySelector('.banner-content.fade-in');
+    if (banner) {
+      setTimeout(() => {
+        banner.classList.add('visible');
+      }, 100); // pequeno atraso para suavidade
+    }
+  });
